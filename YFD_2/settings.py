2c3e8b4c519cb1c95a572eb0f10763c9c73bfbe5
Suraj Pai <surajballambat@gmail.com>
2016-08-25 23:52:42 +0530

    Update settings.py

--- YFD_2/settings.py
+++ YFD_2/settings.py
@@ -122,3 +122,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
